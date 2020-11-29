845c6e57c61132ec55762ae056be03f45591712b
Sami <darthmas00@gmail.com>
2020-11-29 23:46:20 +0330

    Registering models in admin site

--- supermarket/market/admin.py
+++ supermarket/market/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from market.models import Product, Order, OrderRow, Customer
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderRow)
+admin.site.register(Customer)
