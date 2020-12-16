f51285ee65e7859e3053c3b5f4409ab421cb5612
sami <miladian.samad@gmail.com>
2020-12-16 05:49:30 +0330

    finish the base model

--- supermarket/market/models.py
+++ supermarket/market/models.py
@@ -1,38 +1,53 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Product(models.Model):
-    code = models.CharField(max_length=10)
+    code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
-    inventory = models.PositiveIntegerField()
+    inventory = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
     
     def increase_inventory(self, amount):
-        pass
+        self.inventory += amount
+        self.save()
 
     def decrease_inventory(self, amount):
-        pass
+        if self.inventory < amount:
+            raise Exception('The Min of inventory is 0')
+        else:
+            self.inventory -= amount
+            self.save()
 
 class Customer(models.Model):
-    user = None
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     phone = models.CharField(max_length=20)
     address = models.TextField()
-    balance = models.IntegerField()
+    balance = models.IntegerField(default=20000)
 
+    def __str__(self):
+        name = self.user.username
+        return name
+        
     def deposit(self, amount):
-        pass
+        self.balance += amount
+        self.save()
 
     def spend(self, amount):
-        pass
+        if self.balance < amount:
+            raise Exception('The Min of balance is 0')
+        else:
+            self.balance -= amount
+            self.save()
 
 
 class OrderRow(models.Model):
-    product = None
-    order = None
+    product = models.ForeignKey('Product', on_delete=models.PROTECT)
+    order = models.ForeignKey('Order', on_delete=models.PROTECT, related_name='rows')
     amount = models.IntegerField()
 
 
@@ -43,10 +58,10 @@ class Order(models.Model):
     STATUS_CANCELED = 3
     STATUS_SENT = 4
     
-    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    customer = models.ForeignKey('Customer', on_delete=models.PROTECT)
     order_time = models.DateTimeField()
     total_price = models.IntegerField()
-    status = None
+    status = models.IntegerField()
 
     @staticmethod
     def initiate(customer):
