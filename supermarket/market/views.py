3793b3cc5a3b305af30f71354b251efebbeec2ea
sami <miladian.samad@gmail.com>
2020-12-23 16:13:14 +0330

    create a API for this website

--- supermarket/market/views.py
+++ supermarket/market/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
+"""
+    You can define utility functions here if needed
+    For example, a function to create a JsonResponse
+    with a specified status code or a message, etc.
+
+    DO NOT FORGET to complete url patterns in market/urls.py
+"""
+
+
+
 # Create your views here.
+def product_insert(request):
+    # hint: you should check request method like below
+    if request.method != 'POST':
+        pass  # return appropriate error message
+    pass  # main logic and return normal response
