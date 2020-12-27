bf7956bc1115dc240ff7fce2fd449bcfc194a7ec
sami <miladian.samad@gmail.com>
2020-12-27 20:18:36 +0330

    create a new path in urls.py for customers

--- supermarket/market/urls.py
+++ supermarket/market/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # products urls
     path('product/insert/', views.product_insert, name='product_insert'),
     path('product/list/', views.product_list, name='product_list'),
+    path('customer/insert/', views.customer_insert, name='customer_insert'),
     # TODO: insert other url paths
     # path(...
     # path(...
