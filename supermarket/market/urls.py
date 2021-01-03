fec10cf36a37f5fb73c6e35a16ae27184eecca58
sami <miladian.samad@gmail.com>
2021-01-03 19:54:49 +0330

    basic API for products

--- supermarket/market/urls.py
+++ supermarket/market/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # products urls
     path('product/insert/', views.product_insert, name='product_insert'),
     path('product/list/', views.product_list, name='product_list'),
+    path('product/<int:product_id>/', views.product_details, name='product_details'),
+    path('product/<int:product_id>/edit_inventory/', views.product_edit, name='product_edit'),
     path('customer/insert/', views.customer_insert, name='customer_insert'),
     # TODO: insert other url paths
     # path(...
